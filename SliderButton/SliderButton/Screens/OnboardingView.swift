95b707481c7866f82e3536e0adfbfdcdab0dfee1
Uzay Altıner <92029942+uzayaltiner@users.noreply.github.com>
2023-05-25 18:33:58 +0300

    Drag Gesture added to button

--- SliderButton/SliderButton/Screens/OnboardingView.swift
+++ SliderButton/SliderButton/Screens/OnboardingView.swift
@@ -12,6 +12,9 @@ struct OnboardingView: View {
 
     @AppStorage("onboarding") var isOnboardingViewActivate: Bool = true
 
+    @State var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State var buttonOffset: CGFloat = 0
+
     // MARK: - BODY
 
     var body: some View {
@@ -67,7 +70,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
                     }
 
@@ -83,13 +86,27 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActivate = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0, buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActivate = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                }
+                        )
                         Spacer()
                     }
                 }
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             }
         }
