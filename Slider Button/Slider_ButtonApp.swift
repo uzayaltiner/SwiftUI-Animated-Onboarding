36385236f8539f6fc5e880cf81e08ba94a2177c6
Uzay Altıner <92029942+uzayaltiner@users.noreply.github.com>
2023-05-25 13:31:37 +0300

    Initial Commit

---
+++ Slider Button/Slider_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slider_ButtonApp.swift
+//  Slider Button
+//
+//  Created by Uzay Altıner on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Slider_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
